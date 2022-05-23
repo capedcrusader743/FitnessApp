322ea62029bac806921a1260273d41c28c89cc4f
capedcrusader743 <baotin256@gmail.com>
2022-05-23 16:04:15 -0700

    did some stuffs with sign in page and added sign up page

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,47 +1,63 @@
-import React from 'react'
-import './SignIn.css'
-import {
-    onAuthStateChanged,
-    signInWithPopup,
-    GoogleAuthProvider,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    sendPasswordResetEmail,
-    signOut
-} from "firebase/auth";
-import {auth, signInWithGoogle} from "./firebaseConfig.js"
-import { useNavigate } from 'react-router-dom';
-import { initializeApp } from 'firebase/app';
-import { getFirestore, query, getDocs, collection, where, addDoc } from "firebase/firestore";
+import React, { useEffect, useState } from 'react';
+import './SignIn.css';
+import { auth, signInWithGoogle, signInWithEmailAndPassword } from "./firebaseConfig.js";
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 
 function SignIn() {
 
     let navigate = useNavigate();
 
-    const signInWithGoogle = () => {
-        const provider = new GoogleAuthProvider();
-        signInWithPopup(auth, provider)
-            .then((re) => {
-                console.log(re);
-                navigate('/about');
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    }
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [user, loading, error] = useAuthState(auth);
+    useEffect(() => {
+        if (loading) {
+            // maybe trigger a loading screen
+            return;
+        }
+        if (user) navigate('/home');
+    }, [user, loading]);
 
     return (
-    <div className='sign_in'>
-        <h1 className='welcome'>Welcome to SignIn Page</h1>
-        <img className='fitness_icon' src='https://i.pinimg.com/736x/99/ec/1b/99ec1bc01c1c759a9af0f35ed8477531.jpg' alt='' />
-        <h1 className='app_name'>App Name</h1>
-        <p className='motivation_intro'>Are You Ready to Get Shredded?</p>
-        <button onClick={signInWithGoogle}>
-            Sign In
-        </button>
-    </div>
-    )
+        <div className='sign_in'>
+            <h1 className='welcome'>Welcome to SignIn Page</h1>
+            <img className='fitness_icon' src='https://i.pinimg.com/736x/99/ec/1b/99ec1bc01c1c759a9af0f35ed8477531.jpg' alt='' />
+            <h1 className='app_name'>App Name</h1>
+            <p className='motivation_intro'>Are You Ready to Get Shredded?</p>
+            <div className='signin_container'>
+                <input
+                    type="text"
+                    className="signin_textBox"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="E-mail Address"
+                />
+                <input
+                    type="password"
+                    className="signin_textBox"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                />
+                <button
+                    className='signin_btn'
+                    onClick={(e) => signInWithEmailAndPassword(email, password)}
+                >
+                    Login with Email
+                </button>
+                <button className='signin_btn signin_google'
+                    onClick={signInWithGoogle}>
+                    Login with Google
+                </button>
+                <div>
+                    Don't have an account? <Link to='/signup'>Sign Up</Link>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default SignIn
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { GoogleAuthProvider, getAuth, signInWithPopup, createUserWithEmailAndPassword, sendPasswordResetEmail, signOut } from "firebase/auth";
-import { getFirestore, query, getDocs, collection, where, addDoc } from "firebase/firestore";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, signOut } from "firebase/auth";
+import { getFirestore, query, getDocs, collection, where, addDoc, getDoc } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
+import { async } from "@firebase/util";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,6 +24,68 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export {db, auth}
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query((collection(db, "users")), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  }
+  catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+}
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+
+export {db, auth, signInWithEmailAndPassword, signInWithGoogle, registerWithEmailAndPassword, sendPasswordReset, logout}
 
 const analytics = getAnalytics(app);
